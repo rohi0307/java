49729383800ee31af5f820d87535ce5cb78b3fdf
rohi0307 <78890669+rohi0307@users.noreply.github.com>
2021-02-11 08:50:23 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+package com.rc;
+
+public class EvenOdd {
+public boolean evenOdd(int num) {
+	boolean result=false;
+	if(num%2 ==0) {
+		result=true;
+	}else {
+		result=false;
+	}
+	return result;
+}
+}
