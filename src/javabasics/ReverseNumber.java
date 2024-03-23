3f8594f50b27f774793e5beeda0da6fefd70322c
rohi0307 <rohitchavan8180@gmail.com>
2024-03-23 11:40:08 +0530

    Day 2 Correction

--- src/javabasics/ReverseNumber.java
+++ src/javabasics/ReverseNumber.java
@@ -1,14 +1,20 @@
 package javabasics;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
-public static void main(String[] args) {
-	int reverse=0,number=123;
-	while(number!=0)
-	{
-		int reminder=number%10;
-		reverse=reverse*10+reminder;
-		number=number/10;
+	public static void main(String[] args) {
+		System.out.print("Enter a number: ");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+
+		int reverse = 0;
+		while (number != 0) {
+			int reminder = number % 10;
+			reverse = reverse * 10 + reminder;
+			number = number / 10;
+		}
+		System.out.println("Reversed number : " +reverse);
+		sc.close();
 	}
-	System.out.println(reverse);
-}
 }
